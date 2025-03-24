ea74d0ebb65a9472fbadeb87fa22a1d1f505a659
kate0321vs <ekaterina.chechulina2103@gmail.com>
2025-03-25 01:07:23 +0600

    Добавила эндпоинт для получения последних 30 сообщений, а также получения последних сообщений относительно даты

--- api/routers/messages.ts
+++ api/routers/messages.ts
@@ -1,8 +1,28 @@
 import express from 'express';
 import fileDb from "../fileDb";
 import {TMessageWithoutId} from "../type";
+
 const messagesRouter = express.Router();
 
+messagesRouter.get('/', async (req, res) => {
+    const messages = await fileDb.getItems();
+
+    if (req.query.datetime) {
+        const queryDate = req.query.datetime as string;
+        const date = new Date(queryDate);
+
+        if (isNaN(date.getDate())) {
+            res.status(400).send({"error": "Invalid datetime"});
+            return;
+        }
+
+        const filteredMessages = messages.filter(message => new Date(message.datetime) > date);
+        res.send(filteredMessages);
+        return
+    }
+
+    res.send(messages.slice(-30));
+});
 
 messagesRouter.post('/', async (req, res) => {
     if (!req.body.message || !req.body.author) {
