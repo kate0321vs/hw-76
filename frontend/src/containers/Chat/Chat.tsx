be1db9e1878cdb1780e85fe686dcc9eadbd3be5a
kate0321vs <ekaterina.chechulina2103@gmail.com>
2025-03-25 16:11:29 +0600

    Создала клиентскую часть приложения с формой для добавления новых сообщений и отображения всех сообщений

---
+++ frontend/src/containers/Chat/Chat.tsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from 'react';
+import {Messages} from "../../types";
+import UserForm from "../../components/UserForm/UserForm.tsx";
+import UserMessages from "../../components/UserMessages/UserMessages.tsx";
+import axiosApi from "../../../axiosApi.ts";
+
+const Chat = () => {
+  const [userMessages, setUserMessages] = useState<Messages[]>([]);
+  const [lastDateTimeOfMessage, setLastDateTimeOfMessage] = useState<string>('')
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const fetchUrl = lastDateTimeOfMessage ?
+          `/messages?datetime=${lastDateTimeOfMessage}` : '/messages';
+
+        const response = await axiosApi.get(fetchUrl);
+        const posts: Messages[] = response.data;
+
+          if (posts.length > 0) {
+            posts.reverse();
+            setLastDateTimeOfMessage(posts[posts.length - 1].datetime);
+            setUserMessages(prevState => {
+              const newMessages = posts.filter(message => {
+                const existingMessage = prevState.find(prevMessage => prevMessage.id === message.id);
+                return !existingMessage;
+              });
+              return [...newMessages, ...prevState];
+            });
+          }
+      } catch (e) {
+        console.error("Error", e);
+      }
+    };
+
+    void fetchData();
+    const interval = setInterval(async () => {
+      void fetchData()
+    }, 3000);
+    return () => clearInterval(interval);
+
+  }, [lastDateTimeOfMessage]);
+
+  return (
+    <div>
+      <UserForm/>
+      {userMessages.length > 0 ?
+        <>
+          <UserMessages userMessages={userMessages}/>
+        </>
+        :
+        <p className="w-75 mx-auto">No Messages</p>
+      }
+    </div>
+  );
+};
+
+export default Chat;
---
+++ frontend/src/types.d.ts
@@ -0,0 +1,11 @@
+export interface Data {
+  author: string;
+  message: string;
+}
+
+export interface Messages {
+  author: string;
+  message: string;
+  id: string;
+  datetime: string;
+}
